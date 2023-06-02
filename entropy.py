775c4420f389902d1d69f7714057ca3dd02348b6
fbari <thefaisal.bari@gmail.com>
2023-06-01 21:18:58 -0400

    entropy and decision tree

--- entropy.py
+++ entropy.py
@@ -5,4 +5,4 @@ def entropy(data):
     labels, counts = np.unique(data, return_counts=True)
     probabilities = counts / np.sum(counts)
     entropy = -np.sum(probabilities * np.log2(probabilities))
-    return entropy
+    return entropy //
