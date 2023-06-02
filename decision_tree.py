2858e593d1e975aa18c9e2c7f88e4b5d1c200dcf
fbari <thefaisal.bari@gmail.com>
2023-06-01 21:18:39 -0400

    entropy and decision tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,1 +1,9 @@
 # decision tree and splitting criteria
+import numpy as np
+
+class Node:
+    def __init__(self, feature=None, value=None, label=None):
+        self.feature = feature
+        self.value = value
+        self.label = label
+        self.children = {}
--- entropy.py
+++ entropy.py
@@ -1,1 +1,8 @@
 # entropy calculation
+import numpy as np
+
+def entropy(data):
+    labels, counts = np.unique(data, return_counts=True)
+    probabilities = counts / np.sum(counts)
+    entropy = -np.sum(probabilities * np.log2(probabilities))
+    return entropy
