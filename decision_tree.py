da45ef8f7062247bc7f550df87cd72d6f5b064f2
tala Sleman <talasleman@talas-air.home>
2023-06-04 17:09:24 -0400

    classify function

--- decision_tree.py
+++ decision_tree.py
@@ -1,55 +1,74 @@
+# decision tree and splitting criteria
 import numpy as np
 import pandas as pd
 from sklearn import tree
 from sklearn import preprocessing
 import graphviz
 import csv
-
-# import from .csv file
-with open("dataset.csv", "r") as csvfile:
-    csv_reader = csv.reader(csvfile)
-    rows = []
-    for row in csv_reader:
-        rows.append(row)
-
-# concaterate and put everything into a 2D array in numpy format
-dataset = np.array(rows)
-# tabulate and put into pretty table
-table = pd.DataFrame(dataset,
-                     columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'])
-print(table)
-
-# seperate the features from the labels
-x = dataset[:, 0:10]
-y = dataset[:, 10]
-
-# encode all the strings
-encoder = preprocessing.LabelEncoder()
-
-# find length of array
-counter = 0
-for sublist in dataset[0, :]:
-    counter += 1
-
-# encode each string one by one, apparently only takes 1D arrays
-for i in range(counter - 1):
-    x[:, i] = encoder.fit_transform(x[:, i])
-y = encoder.fit_transform(y)
-
-# sets entropy for the tree
-DecisionTree = tree.DecisionTreeClassifier(criterion="entropy")
-
-# builds actual tree
-DecisionTree.fit(x, y)
-# should plot but dont know where it is
-tree.plot_tree(DecisionTree)
+import pydotplus
+from io import StringIO  
+from IPython.display import Image  
+from sklearn.tree import export_graphviz
 
 
-'''
-dot_data = tree.export_graphviz(DecisionTree, out_file=None,
+
+def build_and_train_decision_tree():
+    with open("dataset.csv", "r") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows = []
+        for row in csv_reader:
+            rows.append(row)
+
+    # concaterate and put everything into a 2D array in numpy format
+    dataset = np.array(rows)
+    # tabulate and put into pretty table
+    table = pd.DataFrame(dataset,
+                        columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'])
+    print(table)
+
+    # seperate the features from the labels
+    x = dataset[:, 0:10]
+    y = dataset[:, 10]
+
+    # encode all the strings
+    encoder = preprocessing.OneHotEncoder()
+    x = encoder.fit_transform(x)
+
+    # encode y
+    label_encoder = preprocessing.LabelEncoder()
+    y = label_encoder.fit_transform(y)
+
+    # sets entropy for the tree
+    DecisionTree = tree.DecisionTreeClassifier(criterion="entropy")
+
+    # builds actual tree
+    DecisionTree.fit(x, y)
+    '''
+    # should plot but dont know where it is
+    tree.plot_tree(DecisionTree)
+
+    dot_data = tree.export_graphviz(DecisionTree, out_file=None,
                                 feature_names=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est'],
                                 class_names=encoder.classes_,
                                 filled=True, rounded=True)
-graph = graphviz.Source(dot_data)
-graph.render("mytree1")
-'''
+    graph = graphviz.Source(dot_data)
+    graph.write_png('decision_tree.png')
+    Image(graph.create_png())
+    '''
+    return DecisionTree, encoder
+
+
+def classify_instance(DecisionTree, encoder, instance):
+    # Reshape and encode instance with the encoder
+    instance = np.array(instance).reshape(1, -1)
+    instance = encoder.transform(instance)
+    # Convert sparse matrix to dense
+    instance = instance.toarray()
+    # Predict instance
+    prediction = DecisionTree.predict(instance)
+    # return 'yes' if WillWait == 1 else 'no'
+    return 'Yes' if prediction == 1 else 'No'
+
+
+
+
--- user_interface.py
+++ user_interface.py
@@ -1,102 +1,52 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pandas as pd
+from decision_tree import build_and_train_decision_tree, classify_instance
+from PIL import ImageTk, Image
+import matplotlib
+from matplotlib import pyplot as plt
 
-def browse_files():
-    #open a file dialog to choose data set from
-    #add csv filter
-    filename = filedialog.askopenfilename(initialdir="/",
-                                          filetypes=(("CSV Files", "*.csv"), ("All Files", "*.*")),
-                                          title="Choose a file.")
-    #check if file is empty with panda
-    try:
-        if filename:
-            df = pd.read_csv(filename)
-            print("File Loaded Successfully")
-    except Exception as e:
-        print("Error: ", e)
-        print("Invalid file. Please choose a valid CSV file.")
-        return
+DecisionTree = None
+encoder = None
+
+
+
+def submit():
+    global DecisionTree, encoder
+    inputs = [e.get() for e in entries]
+    
+    result = classify_instance(DecisionTree, encoder, inputs)
+    messagebox.showinfo("Prediction", "WillWait: " + result
+                            )
+    
 
 #initialize GUI
 def run_ui():
+    global DecisionTree, encoder
+    DecisionTree, encoder = build_and_train_decision_tree()
     root = tk.Tk()
 
     root.title("Decision Tree Classifier")
-    root.geometry("500x200")
-    #add button with command
-    browse_button = tk.Button(root, text="Browse files", command=browse_files)
-    #display button
-    browse_button.pack()
+    root.geometry("500x500")
+ 
+    
+    labels = ['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est']
+    
+    global entries
+    entries = []
+    for i, label in enumerate(labels):
+        tk.Label(root, text=label).grid(row=i)
+        entry = tk.Entry(root)
+        entry.grid(row=i, column=1)
+        entries.append(entry)
+  
+    submit_button = tk.Button(root, text="Submit", command=submit)
+    submit_button.grid(row=len(labels)+1, column=1)
 
+    image = Image.open("decision_tree.png")
+    photo = ImageTk.PhotoImage(image)
+    label = tk.Label(root, image=photo)
+    label.image = photo
+    label.grid(row=len(labels)+2, column=0, columnspan=2)
+    
     root.mainloop()
-
-'''
-import tkinter as tk
-from tkinter import filedialog
-import pandas as pd
-
-from entropy import entropy
-from decision_tree import build_and_train_decision_tree
-from classification import classify_instance
-
-
-def browse_files(clf):
-    filename = filedialog.askopenfilename(initialdir="/",
-                                          filetypes=(("CSV Files", "*.csv"), ("All Files", "*.*")),
-                                          title="Choose a file.")
-    try:
-        if filename:
-            df = pd.read_csv(filename)
-            print("File Loaded Successfully")
-            instance = df[0]
-            prediction = predict(clf, instance)
-    except Exception as e:
-        print("Error: ", e)
-        print("Invalid file. Please choose a valid CSV file.")
-        return
-
-def predict(clf, instance):
-    prediction = classify_instance(clf, instance)
-    print(f"The predicted output for the input data is {prediction}")
-
-def print_tree(tree, indent=""):
-    # Assuming the tree is a nested dictionary
-    for feature, branches in tree.items():
-        if isinstance(branches, dict):
-            # This is a subtree, print the feature and recurse
-            print(f"{indent}{feature}?")
-            print_tree(branches, indent + "  ")
-        else:
-            # This is a leaf node, print the outcome
-            print(f"{indent}{branches}")
-
-def run_ui():
-    root = tk.Tk()
-
-    root.title("Decision Tree Classifier")
-    root.geometry("500x200")
-
-    # Load training data and build the decision tree
-    training_data = pd.read_csv('dataset.csv')
-
-    df = pd.read_csv('dataset.csv')
-    X = df.drop('WillWait', axis=1)
-    y = df['WillWait']
-
-    tree = build_and_train_decision_tree(X, y)
-    print_tree(tree)
-
-    instruction_label = tk.Label(root, text="Please browse for a file to predict:")
-    instruction_label.grid(row=0, column=0, sticky=tk.W, pady=4)
-
-    # Browse for new data
-    browse_button = tk.Button(root, text="Browse files", command=lambda: browse_files())
-    browse_button.grid(row=1, column=0, sticky=tk.W, pady=4)
-
-    # Predict button
-    predict_button = tk.Button(root, text="Predict", command=lambda: predict(tree, browse_files()))
-    predict_button.grid(row=2, column=0, sticky=tk.W, pady=4)
-
-    root.mainloop()
-'''
