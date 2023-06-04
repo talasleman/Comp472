84cf3d65f28b6e858bd746039006709fc0385966
fbari <thefaisal.bari@gmail.com>
2023-06-04 19:47:30 -0400

    train and test split

--- decision_tree.py
+++ decision_tree.py
@@ -6,6 +6,8 @@ from sklearn import preprocessing
 import graphviz
 import csv
 
+from sklearn.model_selection import train_test_split
+
 # import from .csv file
 with open("dataset.csv", "r") as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -18,11 +20,17 @@ dataset = np.array(rows)
 # tabulate and put into pretty table
 table = pd.DataFrame(dataset,
                      columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'])
-print(table)
+#print(table)
 
 # seperate the features from the labels
 x = dataset[:, 0:10]
 y = dataset[:, 10]
+split = 0.8
+
+x_train = x[0:int(x.shape[0]*split), :]
+y_train = y[0:int(float(y.size)*split)]
+x_test = x[int(x.shape[0]*split):, :]
+y_test = y[int(float(y.size)*split):]
 
 # encode all the strings
 encoder = preprocessing.LabelEncoder()
@@ -34,20 +42,22 @@ for sublist in dataset[0, :]:
 
 # encode each string one by one, apparently only takes 1D arrays
 for i in range(counter - 1):
-    x[:, i] = encoder.fit_transform(x[:, i])
-y = encoder.fit_transform(y)
+    x_train[:, i] = encoder.fit_transform(x_train[:, i])
+    x_test[:, i] = encoder.fit_transform(x_test[:, i])
+y_train = encoder.fit_transform(y_train)
+y_test = encoder.fit_transform(y_test)
 
 # sets entropy for the tree
-DecisionTree = tree.DecisionTreeClassifier(criterion="entropy")
+DecisionTree = tree.DecisionTreeClassifier(criterion="entropy", random_state=0)
 
 # builds actual tree
 DecisionTree.fit(x, y)
 # should plot but dont know where it is
 tree.plot_tree(DecisionTree)
 
-
 dot_data = tree.export_graphviz(DecisionTree, out_file=None,
-                                feature_names=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est'],
+                                feature_names=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type',
+                                               'Est'],
                                 class_names=encoder.classes_,
                                 filled=True, rounded=True)
 graph = graphviz.Source(dot_data)
