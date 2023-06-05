3c4a549b7e8a993f4c6e1454ed7d2fd20f0199fa
fbari <thefaisal.bari@gmail.com>
2023-06-05 12:24:32 -0400

    decision_tree, line 50 error, once fixed, uncomment lines 77-83

--- decision_tree.py
+++ decision_tree.py
@@ -5,10 +5,10 @@ from sklearn import tree
 from sklearn import preprocessing
 import graphviz
 import csv
-from io import StringIO  
-from IPython.display import Image  
+from io import StringIO
+from IPython.display import Image
+from sklearn.metrics import classification_report
 from sklearn.tree import export_graphviz
-
 
 
 def build_and_train_decision_tree():
@@ -22,40 +22,65 @@ def build_and_train_decision_tree():
     dataset = np.array(rows)
     # tabulate and put into pretty table
     table = pd.DataFrame(dataset,
-                        columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'])
-    print(table)
+                         columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'])
+    # print(table)
+    with open("test.csv", "r") as testfile:
+        csv_reader = csv.reader(testfile)
+        rows = []
+        for row in csv_reader:
+            rows.append(row)
+
+    # concatenate and put everything into a 2D array in numpy format
+    testset = np.array(rows)
+    # tabulate and put into pretty table
+    test_table = pd.DataFrame(testset,
+                              columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est',
+                                       'WillWait'])
 
     # seperate the features from the labels
-    x = dataset[:, 0:10]
-    y = dataset[:, 10]
+    x_train = dataset[:, 0:10]
+    y_train = dataset[:, 10]
+
+    x_test = testset[:, 0:10]
+    y_test = testset[:, 10]
 
     # encode all the strings
     encoder = preprocessing.OneHotEncoder()
-    x = encoder.fit_transform(x)
-    feature_names = encoder.get_feature_names_out(['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est'])
+    x_train = encoder.fit_transform(x_train)
+    #x_test = encoder.fit_transform(x_test) # the problem is here once fixed uncomment the stuff in bottom of function
+
+    feature_names = encoder.get_feature_names_out(
+        ['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est'])
     # encode y
     label_encoder = preprocessing.LabelEncoder()
-    y = label_encoder.fit_transform(y)
+    y_train = label_encoder.fit_transform(y_train)
+    y_test = label_encoder.fit_transform(y_test)
 
     # sets entropy for the tree
-    DecisionTree = tree.DecisionTreeClassifier(criterion="entropy")
+    DecisionTree = tree.DecisionTreeClassifier(criterion="entropy", random_state=0)
 
     # builds actual tree
-    DecisionTree.fit(x, y)
-    
-    # should plot but dont know where it is
+    DecisionTree.fit(x_train, y_train)
+
+    # should plot
     tree.plot_tree(DecisionTree)
 
     dot_data = tree.export_graphviz(DecisionTree, out_file=None,
-                                feature_names=feature_names,
-                                class_names=label_encoder.classes_,
-                                filled=True, rounded=True)
+                                    feature_names=feature_names,
+                                    class_names=label_encoder.classes_,
+                                    filled=True, rounded=True)
     graph = graphviz.Source(dot_data)
-    #create png instead
+    # create png instead
     png_file_path = 'decision_tree'
     graph.format = 'png'
     graph.render(filename=png_file_path, cleanup=True)
-    
+    '''
+    # predict the output based on test file
+    y_pred = DecisionTree.predict(x_test)
+    print("predicted: ", encoder.inverse_transform(y_pred))
+
+    print(classification_report(y_test, y_pred))
+    '''
     return DecisionTree, encoder
 
 
